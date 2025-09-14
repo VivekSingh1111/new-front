6cb2f697201f9cdd91017275b59ca6da527f438a
VivekSingh1111 <vs554432@gmail.com>
2025-09-14 22:52:37 +0530

    Update EmployeeService.js

--- src/service/EmployeeService.js
+++ src/service/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_S_API_BASE_URL = "http://localhost:9090/employees"
+const EMPLOYEE_S_API_BASE_URL = "backend-production-3a7b.up.railway.app"
 class EmployeeService{
     saveEmployee(employee){
         return axios.post(EMPLOYEE_S_API_BASE_URL, employee)
